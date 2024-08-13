a9b445423ec84eee062900ed770f15465715eb16
skuncel <git@mkersten.net>
2024-08-13 22:31:21 +0200

    chore(project): inital commit  🎉

---
+++ Beamcel/BeamcelApp.swift
@@ -0,0 +1,32 @@
+//
+//  BeamcelApp.swift
+//  Beamcel
+//
+//  Created by Marcel Kersten on 13.08.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BeamcelApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
